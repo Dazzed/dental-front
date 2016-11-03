312a848bbd358f52ed147c3c2a617834fa089fa5
Valentin Marinov <dev.valentin2013@gmail.com>
2016-11-04 01:23:28 +0200

    fix lint error, add page title to Manage Members page

--- app/components/SideNav/index.js
+++ app/components/SideNav/index.js
@@ -19,7 +19,7 @@ const navItems = {
   dentist: [
     { text: 'Dentist dashboard', link: '/dashboard' },
     { text: 'Edit office info', link: '/dentist/office/edit' },
-    { text: 'Manage Members', link: '/dentist/family-members' },
+    { text: 'Manage members', link: '/dentist/family-members' },
     // { text: 'Edit membership', link: '/custom-membership' },
     { text: 'Contact support', link: '/temp_link' },
     // { text: 'Reports', link: '/temp_link' },
