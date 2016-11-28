ad32474d10b03864946b7f14b1854d5ddf84013c
Valentin Marinov <dev.valentin2013@gmail.com>
2016-11-28 18:43:55 +0200

    comment out payment due date from user dashboard

--- app/components/UserDashboard/FamilyMembers.js
+++ app/components/UserDashboard/FamilyMembers.js
@@ -74,26 +74,11 @@ class FamilyMembers extends Component {
   }
 
   render () {
-    const { accountStatus, monthlyDue, dueAt, members, owner } = this.props;
+    // const { accountStatus, monthlyDue, dueAt, members, owner } = this.props;
+    const { members, owner } = this.props;
 
     return (
       <Well styleName="family-members-container">
-        <Row styleName="top-header">
-          <Col md={4}>
-            <span styleName="label">Account:</span>
-            <span styleName={`value ${accountStatus}`}>{accountStatus}</span>
-          </Col>
-          <Col md={4}>
-            <span styleName="label">Your total monthly due:</span>
-            <span styleName="value">${monthlyDue}</span>
-          </Col>
-          <Col md={4}>
-            <span styleName="label">Payment due date:</span>
-            { dueAt &&
-              <span styleName="value">{dueAt.format('MMM D, YYYY')}</span>
-            }
-          </Col>
-        </Row>
         <Row styleName="list-header">
           <Col md={3}>Name</Col>
           <Col md={3}>Family Relationshiop</Col>
