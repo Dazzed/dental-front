3798f9ac2a8aef07cb267fe4da6986eb9cbf0c38
Valentin Marinov <dev.valentin2013@gmail.com>
2016-12-08 14:34:24 +0200

    member form layout change

--- app/components/MemberForm/index.js
+++ app/components/MemberForm/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import { Field, reduxForm } from 'redux-form';
+import Row from 'react-bootstrap/lib/Row';
 import Col from 'react-bootstrap/lib/Col';
-import FormGroup from 'react-bootstrap/lib/FormGroup';
 // import Image from 'react-bootstrap/lib/Image';
 
 import { MEMBER_RELATIONSHIP_TYPES } from 'common/constants';
@@ -35,88 +35,96 @@ export default class MemberForm extends React.Component {
     // const defaultAvatar = 'http://www.teenink.com/images/default_face.gif';
 
     return (
-      <form
-        onSubmit={handleSubmit}
-        className="form-horizontal"
-      >
-        {/* <Col md={12}>
-          <Col sm={2}>
-            <Image
-              src={defaultAvatar}
-              rounded
-              responsive
-              style={{ height: '100px' }}
-            />
-          </Col>
-          <Col sm={10} style={{ marginTop: '70px', marginLeft: '-30px' }}>
-            <Field
-              name="avatar"
-              type="file"
-              label="avatar"
-              component={Input}
-            />
-          </Col>
-        </Col> */}
-
-        <Field
-          name="firstName"
-          type="text"
-          component={LabeledInput}
-          label="First Name"
-          className="col-md-6"
-        />
-
-        <Field
-          name="lastName"
-          type="text"
-          component={LabeledInput}
-          label="Last Name"
-          className="col-md-6"
-        />
-
-        <Field
-          name="familyRelationship"
-          type="select"
-          component={LabeledInput}
-          label="Family Relationship"
-          className="col-md-6"
+      <Col md={12}>
+        <form
+          onSubmit={handleSubmit}
+          className="form-horizontal"
         >
-          <option value="">Select a relationship type</option>
-          {Object.keys(MEMBER_RELATIONSHIP_TYPES).map((key, index) =>
-            <option value={key} key={index}>
-              {MEMBER_RELATIONSHIP_TYPES[key]}
-            </option>
-          )}
-        </Field>
+          {/* <Col md={12}>
+            <Col sm={2}>
+              <Image
+                src={defaultAvatar}
+                rounded
+                responsive
+                style={{ height: '100px' }}
+              />
+            </Col>
+            <Col sm={10} style={{ marginTop: '70px', marginLeft: '-30px' }}>
+              <Field
+                name="avatar"
+                type="file"
+                label="avatar"
+                component={Input}
+              />
+            </Col>
+          </Col> */}
 
-        <Field
-          name="birthDate"
-          type="date"
-          component={renderDatePicker}
-          label="Birthdate"
-          className="col-md-6"
-        />
+          <Row>
+            <Field
+              name="firstName"
+              type="text"
+              component={LabeledInput}
+              label="First Name"
+              className="col-md-6"
+            />
 
-        <Field
-          name="phone"
-          type="text"
-          mask="(999) 999-9999"
-          maskChar=" "
-          component={LabeledInput}
-          label="Phone Number"
-          className="col-md-6"
-        />
+            <Field
+              name="lastName"
+              type="text"
+              component={LabeledInput}
+              label="Last Name"
+              className="col-md-6"
+            />
+          </Row>
 
-        <Field
-          name="email"
-          type="text"
-          component={LabeledInput}
-          label="Email"
-          className="col-md-6"
-        />
+          <Row>
+            <Field
+              name="familyRelationship"
+              type="select"
+              component={LabeledInput}
+              label="Family Relationship"
+              className="col-md-6"
+            >
+              <option value="">Select a relationship type</option>
+              {Object.keys(MEMBER_RELATIONSHIP_TYPES).map((key, index) =>
+                <option value={key} key={index}>
+                  {MEMBER_RELATIONSHIP_TYPES[key]}
+                </option>
+              )}
+            </Field>
+          </Row>
 
-        <Col sm={12}>
-          <FormGroup>
+          <Row>
+            <Field
+              name="birthDate"
+              type="date"
+              component={renderDatePicker}
+              label="Birthdate"
+              className="col-md-6"
+            />
+          </Row>
+
+          <Row className="form-group">
+            <Field
+              name="phone"
+              type="text"
+              mask="(999) 999-9999"
+              maskChar=" "
+              component={LabeledInput}
+              label="Phone Number"
+              className="col-md-6"
+            />
+
+            <Field
+              name="email"
+              type="text"
+              component={LabeledInput}
+              label="Email"
+              className="col-md-6"
+            />
+          </Row>
+
+          <Row>
             <Col sm={12}>
               <button
                 type="submit"
@@ -126,10 +134,10 @@ export default class MemberForm extends React.Component {
                 Save
               </button>
             </Col>
-          </FormGroup>
-        </Col>
+          </Row>
 
-      </form>
+        </form>
+      </Col>
     );
   }
 }
