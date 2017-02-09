58dccb20b282e241247ca9c8c447dec7be5684e1
jtfairbank <jtfairbank@gmail.com>
2017-02-09 02:10:51 -0800

    Touchup the dentist learn-more page.

--- app/containers/LearnMorePage/index.js
+++ app/containers/LearnMorePage/index.js
@@ -13,7 +13,9 @@ import React from 'react';
 import CSSModules from 'react-css-modules';
 import FaAngleLeft from 'react-icons/lib/fa/angle-left';
 import FaAngleRight from 'react-icons/lib/fa/angle-right';
+import { connect } from 'react-redux';
 import { Link } from 'react-router';
+import { push } from 'react-router-redux';
 
 // app
 import PageHeader from 'components/PageHeader';
@@ -22,11 +24,27 @@ import PageHeader from 'components/PageHeader';
 import styles from './styles.css';
 
 /*
+Redux
+------------------------------------------------------------
+*/
+function mapDispatchToProps (dispatch) {
+  return {
+    changeRoute: (url) => dispatch(push(url)),
+  };
+}
+
+
+/*
 Learn More
 ================================================================================
 */
+@connect(null, mapDispatchToProps)
 @CSSModules(styles)
 export default class LearnMorePage extends React.Component {
+  goToDentistSignup = () => {
+    this.props.changeRoute('/accounts/dentist-signup');
+  }
+
   render() {
     return (
       <div styleName="container-wrapper">
@@ -57,25 +75,15 @@ export default class LearnMorePage extends React.Component {
         ------------------------------------------------------------
         */}
         <div styleName="second-block">
-          <div styleName="arrow-left">
-            <FaAngleLeft size={96} />
-          </div>
+          <h2 styleName="large-title">
+            Why DentalHQ.com?
+          </h2>
 
-          <div styleName="second-block__content">
-            <h2 styleName="large-title">
-              Why DentalHQ.com?
-            </h2>
-
-            <p styleName="large-text">
-              Cut out the middleman and attract new fee-for-service
-              <br />
-              patients without the hassle of dental insurance.
-            </p>
-          </div>
-
-          <div styleName="arrow-right">
-            <FaAngleRight size={96} />
-          </div>
+          <p styleName="large-text">
+            Cut out the middleman and attract new fee-for-service
+            <br />
+            patients without the hassle of dental insurance.
+          </p>
         </div>
 
         {/*
@@ -92,7 +100,12 @@ export default class LearnMorePage extends React.Component {
               and we'll walk you through the process!
             </p>
 
-            <input type="button" styleName="large-button__inverse" value="GET STARTED &gt;" />
+            <input
+              type="button"
+              styleName="large-button__inverse"
+              value="GET STARTED &gt;"
+              onClick={this.goToDentistSignup}
+            />
           </div>
         </div>
 
