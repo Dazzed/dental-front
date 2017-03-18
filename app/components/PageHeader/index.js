7244d66f86fb3785f13803dc0601872dadcea085
jtfairbank <jtfairbank@gmail.com>
2017-03-17 18:39:24 -0700

    Add header nav items for logged in users.

--- app/components/PageHeader/index.js
+++ app/components/PageHeader/index.js
@@ -11,36 +11,80 @@ Imports
 // libs
 import React, { PropTypes } from 'react';
 import CSSModules from 'react-css-modules';
-import Header from 'react-bootstrap/lib/PageHeader';
-import Grid from 'react-bootstrap/lib/Grid';
-import Col from 'react-bootstrap/lib/Col';
+import { Link } from 'react-router';
 
 // local
 import styles from './styles.css';
+
+const dashboardPages = [
+  '/dentist/members',
+  '/dentist/new-members',
+  '/dentist/new-reviews',
+
+  '/your-dentist',
+  '/your-family',
+  '/your-profile',
+];
 
 
 /*
 Page Header
 ================================================================================
 */
-function PageHeader ({ borderContent, children, title, userType }) {
+function PageHeader ({ borderContent, children, pathname, title, user }) {
   let content = children;
   if (title) {
     content = (<h1 styleName="large-title">{title}</h1>);
   }
 
+  if (user && user.type) {
+    borderContent = (
+      <div styleName="border-content--user">
+        <div className="pull-left">
+          Welcome
+          {' '}
+          <span styleName="user-name">
+            {user.firstName} {user.lastName}
+          </span>
+        </div>
+
+        <div className="pull-right">
+          <span styleName={"header-link" + (dashboardPages.indexOf(pathname) > -1 ? " header-link--current" : "")}>
+            <Link to="/dentist/new-members">Dashboard</Link>
+          </span>
+          |
+          <span styleName="header-link">
+            <Link to="/faq">Help &amp; FAQ</Link>
+          </span>
+          |
+          <span styleName="header-link">
+            <Link to="/contact-admin">Contact Admin</Link>
+          </span>
+          |
+          <span styleName="header-link">
+            <Link to="/accounts/logout">Logout</Link>
+          </span>
+        </div>
+      </div>
+    );
+  }
+
   return (
     <div>
       <div styleName="page-header">
-        <div styleName={`page-header__overlay`}>
-          <div className="container">
-            {content}
+        {(!user || !user.type) && (
+          <div styleName="page-header__overlay">
+            <div className="container">
+              {content}
+            </div>
           </div>
-        </div>
+        )}
       </div>
 
       <div styleName="border-content">
-        {borderContent}
+        <div className="container">
+          {borderContent}
+        </div>
       </div>
     </div>
   );
@@ -51,8 +95,13 @@ Props
 ------------------------------------------------------------
 */
 PageHeader.propTypes = {
+  borderContent: React.PropTypes.node,
+  pathname: React.PropTypes.string,
   title: PropTypes.string,
-  userType: PropTypes.string,
+  user: React.PropTypes.oneOfType([
+    React.PropTypes.bool, // will be `false` until loaded
+    React.PropTypes.object,
+  ]),
 };
 
 export default CSSModules(styles, { allowMultiple: true })(PageHeader);
