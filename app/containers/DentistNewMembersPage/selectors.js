5f89472623e2491ff479b5c6756b41309ebd17b4
jtfairbank <jtfairbank@gmail.com>
2017-03-27 02:28:33 -0700

    Make sure to wait for the dentistInfo before displaying the dentist dashboard.

--- app/containers/DentistNewMembersPage/selectors.js
+++ app/containers/DentistNewMembersPage/selectors.js
@@ -14,6 +14,7 @@ import { createSelector } from 'reselect';
 // app
 import { selectCurrentUser } from 'containers/App/selectors';
 import {
+  selectDentistInfo,
   selectPatients,
   selectProcessedPatients,
 } from 'containers/DentistMembersPage/selectors';
@@ -61,9 +62,10 @@ Data Loaded
 */
 const selectDataLoaded = createSelector(
   selectCurrentUser,
+  selectDentistInfo,
   selectPatients,
-  (user, patients) => {
-    return user !== false && patients !== null;
+  (user, dentistInfo, patients) => {
+    return user !== false && dentistInfo !== null && patients !== null;
   }
 );
  
