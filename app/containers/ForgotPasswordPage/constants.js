bc82a74c5378caefb64b7bd754b4ca9bcc86b49c
Godson Ukpere <godson.ukpere@gmail.com>
2017-06-15 17:36:00 +0100

    chore(ForgotPassword): Create page

---
+++ app/containers/ForgotPasswordPage/constants.js
@@ -0,0 +1,15 @@
+/*
+Login Page Constants
+================================================================================
+*/
+
+/* eslint-disable no-multi-spaces */
+
+/*
+Login Constants
+------------------------------------------------------------
+*/
+export const FORGOT_PASSWORD_REQUEST = 'LoginPage/FORGOT_PASSWORD_REQUEST';
+export const FORGOT_PASSWORD_SUCCESS = 'LoginPage/FORGOT_PASSWORD_SUCCESS';
+export const FORGOT_PASSWORD_ERROR = 'LoginPage/FORGOT_PASSWORD_ERROR';
+export const LOGOUT = 'LoginPage/LOGOUT';
