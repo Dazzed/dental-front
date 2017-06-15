92368a92d9dbef9b19450fca6fffe1858ef74709
Godson Ukpere <godson.ukpere@gmail.com>
2017-06-15 17:35:51 +0100

    chore(ForgotPassword): Create form

---
+++ app/components/ForgotPasswordForm/index.js
@@ -0,0 +1,82 @@
+/*
+Login Form Component
+================================================================================
+*/
+
+/*
+Imports
+------------------------------------------------------------
+*/
+// libs
+import React from 'react';
+import Col from 'react-bootstrap/lib/Col';
+import FormGroup from 'react-bootstrap/lib/FormGroup';
+import HelpBlock from 'react-bootstrap/lib/HelpBlock';
+import CSSModules from 'react-css-modules';
+import Row from 'react-bootstrap/lib/Row';
+import { Field, reduxForm } from 'redux-form';
+
+// app
+import LabeledInput from 'components/LabeledInput';
+
+// local
+import forgotPasswordFormValidator from './validator';
+import styles from './styles.css';
+
+/*
+Login Form
+================================================================================
+*/
+@reduxForm({
+  form: 'forot_password',
+  validate: forgotPasswordFormValidator,
+})
+@CSSModules(styles)
+class ForgotPasswordForm extends React.Component {
+
+  static propTypes = {
+    error: React.PropTypes.object,
+    handleSubmit: React.PropTypes.func.isRequired,
+    submitting: React.PropTypes.bool.isRequired,
+  };
+
+  getLabeledInput(props) {
+    return new LabeledInput(props);
+  }
+
+  render() {
+    const { error, handleSubmit, submitting } = this.props;
+
+    return (
+      <form
+        onSubmit={handleSubmit}
+        className="form-horizontal"
+        styleName="wrapper"
+      >
+
+        <Row>
+          <Field
+            name="email"
+            type="text"
+            component={this.getLabeledInput}
+            label="Email"
+            placeholder="Email"
+          />
+
+        </Row>
+
+        <FormGroup className="has-error">
+          <Col sm={12}>
+            {error && <HelpBlock>{error}</HelpBlock>}
+          </Col>
+        </FormGroup>
+
+        <div className="text-center">
+          <input type="submit" disabled={submitting} styleName="button" value="Reset Password &gt;" />
+        </div>
+      </form>
+    );
+  }
+}
+
+export default ForgotPasswordForm;
