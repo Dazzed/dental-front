82ae072e9c66f4fac9dd22663164ae70991883c0
John <john@dentalhq.com>
2017-10-29 18:09:52 +0000

    Update the footer discliamer text and add it to the homepage and the learn-more page.

--- app/components/Footer/index.js
+++ app/components/Footer/index.js
@@ -53,7 +53,7 @@ function Footer (props) {
 
       {props.showDisclaimer && (
         <p styleName="disclaimer">
-          DentalHQ is an innovative directory of dental offices offering In house Dental Membership Plans. Dental Membership plans ARE NOT DENTAL INSURANCE. The dental membership plan is a package of services that the dental offices have chosen to offer for the set prices listed. These plans are between you and your selected dentist and are active immediately upon payment with no waiting periods. Payments made through the DentalHQ signup process go directly to the dentist. You may cancel your membership for any reason after 90 days.
+          DentalHQ is an innovative directory of dental offices offering In-House Dental Membership Plans. Dental Membership plans ARE NOT DENTAL INSURANCE. The dental membership plan is a package of services that the dental offices have chosen to offer for the set prices listed. These plans are between you and your selected dentist and are active immediately upon payment with no waiting periods. Payments made through the DentalHQ signup process go directly to the dentist. You may cancel your membership for any reason after 90 days. Cancellation for a full refund is allowed before 90 days by contacting your dentist. However payment in full directly to your dentist for any services utilized during this period will be required at the time of cancellation and refund.
         </p>
       )}
 
