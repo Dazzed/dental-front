cc6fdf643a66c7316a27cef515c095e2714c1c8c
dazzed <sameepsheth@gmail.com>
2017-10-31 09:06:11 +0000

    Display child and adult savings properly

--- app/containers/SignupPatientOffsitePage/selectors.js
+++ app/containers/SignupPatientOffsitePage/selectors.js
@@ -181,6 +181,48 @@ const isSignedUpSelector = createSelector(
   (substate) => substate.patientCreated,
 );
 
+const dentistSavingsSelector = createSelector(
+  domainSelector,
+  (substate) => {
+    try {
+      const { dentist } = substate;
+      const { priceCodes } = dentist.dentistInfo;
+      const priceCodeObject = convertPriceCodeArrayToObject(priceCodes);
+      const {
+        D1110,
+        D0120,
+        D0274,
+        D0330,
+        D0220,
+        D0140,
+        D1120,
+        D0272,
+        D1206
+      } = priceCodeObject;
+      let adultSavings = (D1110 * 2) + (D0120 * 2) + D0274 + (D0330 * 0.3) + D0220 + D0140;
+      let childSavings = (D1120 * 2) + (D0120 * 2) + D0272 + (D0330 * 0.3) + D0220 + D0140 + D1206;
+      const { memberships } = dentist;
+      const standardAnnualMembership = memberships
+        .find(m => m.name === 'standard annual membership' && m.active);
+      const standardAnnualChildMembership = memberships
+        .find(m => m.name === 'standard annual child membership' && m.active);
+      adultSavings -= parseFloat(standardAnnualMembership.price);
+      childSavings -= parseFloat(standardAnnualChildMembership.price);
+      if (adultSavings < 0) adultSavings = 0;
+      if (childSavings < 0) childSavings = 0;
+      return {
+        adult: adultSavings.toFixed(2),
+        child: childSavings.toFixed(2)
+      };
+    } catch (e) {
+      return {
+        adult: 'N/A',
+        child: 'N/A'
+      };
+    }
+  }
+);
+
 /*
 Export
 ------------------------------------------------------------
@@ -209,17 +251,27 @@ export {
   // signup
   accountInfoSelector,
   isSignedUpSelector,
+  dentistSavingsSelector
 }
 
-function replaceDefaultToStandard(memberships) {
+function replaceDefaultToStandard (memberships) {
   if (memberships) {
     return memberships.map(m => {
       return {
         ...m,
-        name: m.name.replace('default','standard')
+        name: m.name.replace('default', 'standard')
       };
     });
   } else {
     return memberships;
   }
+}
+
+function convertPriceCodeArrayToObject (priceCodes) {
+  return priceCodes
+    .reduce((acc, priceCode) => {
+      const { code, price } = priceCode;
+      acc[`D${code}`] = parseFloat(price);
+      return acc;
+    }, {});
 }
